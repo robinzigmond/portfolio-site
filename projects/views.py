244a718978853c786684695303aef223ce3f3376
Robin Zigmond <robinzig@hotmail.com>
2017-08-19 03:04:54 +0100

    started basic homepage

--- projects/views.py
+++ projects/views.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+import models
 
 # Create your views here.
+def home(request):
+    tech = models.Tech.objects.all()
+    return render(request, "home.html", {"active": "home", "techlist": tech})
